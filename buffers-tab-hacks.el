239c342d5b43826c683ef1527afee99f4e3713d0
Stephen J. Turnbull <stephen@xemacs.org>
2016-06-17 22:24:26 +0900

    Initialize buffers-tab-hacks and .gitignore.

---
+++ buffers-tab-hacks.el
@@ -0,0 +1,34 @@
+;;; buffers-tab-hacks.el --- Stephen's hacks for the buffers tab control
+
+;; Copyright (C) 2016 Stephen J. Turnbull
+
+;; Author: Stephen J. Turnbull <stephen@xemacs.org>
+;; Created: 2016-06-17
+;; Keywords: tab control, buffer list, gutter
+
+;; This file is not part of XEmacs, but it is specific to XEmacs.
+
+;; This file is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 2 of the License, or (at your
+;; option) any later version.  Other terms may be available from the author.
+
+;; This file is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+;; for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with XEmacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Synched up with: Not in GNU.
+ 
+;;; Commentary:
+
+;; PLACE YOUR COMMENTARY (DESCRIPTION OF THE LIBRARY'S CONTENTS AND FUNCTION)
+;; IN THIS SECTION.
+
+;;; Code:
+
+
+;;; buffers-tab-hacks.el ends here
