4e01196bdc5ef3b67c9a73bc3ee62c3d36a9cdfb
Stephen J. Turnbull <stephen@xemacs.org>
2016-07-11 16:00:55 +0900

    Add all the code.

--- buffers-tab-hacks.el
+++ buffers-tab-hacks.el
@@ -25,10 +25,51 @@
  
 ;;; Commentary:
 
-;; PLACE YOUR COMMENTARY (DESCRIPTION OF THE LIBRARY'S CONTENTS AND FUNCTION)
-;; IN THIS SECTION.
+;; Various hacks for VM.
+;;
+;; 
 
 ;;; Code:
 
+(defun buffers-tab-omit-vm-nonsummary (candidate ignore)
+  "Return nil if CANDIDATE is a VM buffer, but not a Summary buffer.
 
+Also ignore empty folder Summary buffers.
+When an element of `buffers-tab-filter-functions', omit such buffers."
+  (or (and (eq (symbol-value-in-buffer 'major-mode candidate) 'vm-summary-mode)
+	   (> (length (symbol-value-in-buffer 'vm-message-list candidate)) 0))
+      (not (memq (symbol-value-in-buffer 'major-mode candidate)
+		 '(vm-mode vm-presentation-mode vm-virtual-mode vm-summary-mode)))))
+
+(defun buffers-tab-sort-by-folder-size (buffers)
+  (sort* buffers #'< :key (lambda (buffer)
+			    (length (symbol-value-in-buffer 'vm-message-list
+							    buffer)))))
+
+(defun sjt/test-buffer-handling ()
+  (interactive)
+  (let ((l (buffer-list))
+	(b (get-buffer "INBOX Summary")))
+    (unless b (error 'invalid-state "no INBOX folder"))
+    (setq l (delete-if-not (lambda (x)
+			     (and (buffers-tab-omit-vm-nonsummary x b)
+				  (select-buffers-tab-buffers-by-mode x b)))
+			   l))
+    (setq l (mapcar (lambda (bb)
+		      (list (length (symbol-value-in-buffer 'vm-message-list
+							    bb))
+			    (substring (buffer-name bb) 0 6)))
+		    (buffers-tab-sort-by-folder-size l)))
+    l))
+
+(defun buffers-tab-truncate-vm-summary-name (buffer)
+  "Remove \" Summary\" from VM summary buffer names.
+
+Return the result of calling `format-buffers-tab-line' on the value."
+  (let ((name (format-buffers-tab-line buffer)))
+    (if (eq 'vm-summary-mode (symbol-value-in-buffer 'major-mode buffer))
+	(substring name 0 -8)
+      name)))
+
+(provide 'buffers-tab-hacks)
 ;;; buffers-tab-hacks.el ends here
