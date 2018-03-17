c34f89b3bbb964d11734b80caf3840954ef8b459
Stephen J. Turnbull <stephen@xemacs.org>
2018-03-17 16:52:21 +0900

    Major overhaul.

--- buffers-tab-hacks.el
+++ buffers-tab-hacks.el
@@ -36,7 +36,10 @@
 
 Also ignore empty folder Summary buffers.
 When an element of `buffers-tab-filter-functions', omit such buffers."
-  (or (and (eq (symbol-value-in-buffer 'major-mode candidate) 'vm-summary-mode)
+  (or (null candidate)			; #### shouldn't be needed!
+                                        ; Why are we passed nil here? See also
+                                        ; #'buffers-tab-sort-by-folder-date.
+      (and (eq (symbol-value-in-buffer 'major-mode candidate) 'vm-summary-mode)
 	   (> (length (symbol-value-in-buffer 'vm-message-list candidate)) 0))
       (not (memq (symbol-value-in-buffer 'major-mode candidate)
 		 '(vm-mode vm-presentation-mode vm-virtual-mode vm-summary-mode)))))
@@ -46,8 +49,44 @@ When an element of `buffers-tab-filter-functions', omit such buffers."
 			    (length (symbol-value-in-buffer 'vm-message-list
 							    buffer)))))
 
-(defun sjt/test-buffer-handling ()
-  (interactive)
+;; In fact, this seems quite fast enough, with 43 VM buffers.
+;; #### We can speed this up by computing timestamps and saving them in
+;; buffer-locals before sorting.
+;; #### We can speed this up more by checking timestamps at mail getting time.
+(defun sjt/vm-youngest-date-of-folder (buffer)
+  (with-current-buffer buffer
+    (let ((date (vm-so-sortable-datestring (car vm-message-list)))
+	  date2)
+      (loop
+	for msg in (cdr vm-message-list)
+	do
+	(setq date2 (vm-so-sortable-datestring msg))
+	(when (string< date date2)
+	  (setq date date2)))
+      date)))
+
+(defun buffers-tab-sort-by-folder-date (buffers)
+  "Sort by the date of the youngest message in the folder."
+  (if (and buffers			; #### shouldn't be needed!
+                                        ; Why are we passed nil here? See also
+                                        ; #'buffers-tab-omit-vm-nonsummary.
+	   (eq (symbol-value-in-buffer 'major-mode (car buffers))
+	       'vm-summary-mode))
+      ;; Don't use #'string>, it's a macro from bbdb.
+      (nreverse (sort* buffers #'string< :key #'sjt/vm-youngest-date-of-folder))
+    (sort* buffers #'>
+	   :key (lambda (buffer)
+		  (with-current-buffer buffer
+		    ;; #'visited-file-modtime should return zero if unvisited.
+		    (car (visited-file-modtime)))))))
+
+(defun sjt/test-buffer-handling (&optional func)
+  (interactive "S")
+  (if func
+      (unless (member func '(buffers-tab-sort-by-folder-size
+				buffers-tab-sort-by-folder-date))
+	(error 'wrong-type-argument "buffers-tab sort function" func))
+    (setq func buffers-tab-sort-function))
   (let ((l (buffer-list))
 	(b (get-buffer "INBOX Summary")))
     (unless b (error 'invalid-state "no INBOX folder"))
@@ -56,11 +95,15 @@ When an element of `buffers-tab-filter-functions', omit such buffers."
 				  (select-buffers-tab-buffers-by-mode x b)))
 			   l))
     (setq l (mapcar (lambda (bb)
-		      (list (length (symbol-value-in-buffer 'vm-message-list
-							    bb))
+		      (list (cond ((eq func #'buffers-tab-sort-by-folder-size)
+				   (length (symbol-value-in-buffer
+					    'vm-message-list
+					    bb)))
+				  ((eq func #'buffers-tab-sort-by-folder-date)
+				   (sjt/vm-youngest-date-of-folder bb)))
 			    (substring (buffer-name bb) 0 6)))
-		    (buffers-tab-sort-by-folder-size l)))
-    l))
+		    (funcall func l)))
+    (cons func l)))
 
 (defun buffers-tab-truncate-vm-summary-name (buffer)
   "Remove \" Summary\" from VM summary buffer names.
@@ -70,6 +113,53 @@ Return the result of calling `format-buffers-tab-line' on the value."
     (if (eq 'vm-summary-mode (symbol-value-in-buffer 'major-mode buffer))
 	(substring name 0 -8)
       name)))
+
+(defun sjt/switch-to-buffer-list-excludes ()
+  "List buffers which are VM buffers but not Summary buffers."
+  (delete-if (lambda (x) (buffers-tab-omit-vm-nonsummary x x)) (buffer-list)))
+
+(defvar sjt/switch-to-buffer-exclude-list-function
+  #'current-buffer
+  "Return list of buffers excluded from completion in `sjt/switch-to-buffer'.
+
+Default is `current-buffer' for backward compatibility.")
+
+(defun sjt/switch-to-buffer (bufname &optional norecord)
+  "Select buffer BUFNAME in the current window.
+BUFNAME may be a buffer or a buffer name and is created if it did not exist.
+Optional second arg NORECORD non-nil means do not put this buffer at the
+front of the list of recently selected ones.
+
+WARNING: This is NOT the way to work on another buffer temporarily
+within a Lisp program!  Use `set-buffer' instead.  That avoids messing with
+the window-buffer correspondences."
+  (interactive
+   (list (read-buffer "Switch to buffer: "
+		      (other-buffer (current-buffer))
+		      nil
+		      (cons (current-buffer)
+			    (funcall
+			     sjt/switch-to-buffer-exclude-list-function)))))
+  ;; #ifdef I18N3
+  ;; #### Doc string should indicate that the buffer name will get
+  ;; translated.
+  ;; #endif
+  (if (eq (minibuffer-window) (selected-window))
+      (error "Cannot switch buffers in minibuffer window"))
+  (if (window-dedicated-p (selected-window))
+      (error "Cannot switch buffers in a dedicated window"))
+  (let (buf)
+    (if (null bufname)
+	(setq buf (other-buffer (current-buffer)))
+      (setq buf (get-buffer bufname))
+      (if (null buf)
+	  (progn
+	    (setq buf (get-buffer-create bufname))
+	    (set-buffer-major-mode buf))))
+    (push-window-configuration)
+    (set-buffer buf)
+    (set-window-buffer (last-nonminibuf-window) buf norecord)
+    buf))
 
 (provide 'buffers-tab-hacks)
 ;;; buffers-tab-hacks.el ends here
